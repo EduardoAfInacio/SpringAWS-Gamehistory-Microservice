b78bca5d242147bbe05f00c6f5304343aa2244d3
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-12 13:13:23 -0300

    first commit

---
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/SpringAwsGamehistoryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.SpringAWS_Gamehistory_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsGamehistoryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsGamehistoryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/SpringAwsGamehistoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoinacio.SpringAWS_Gamehistory_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsGamehistoryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
