6e406ddc9031c46b2a03a0d6066a83b83ade442d
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-12 13:56:23 -0300

    Creating DynamoDB Configuration class.

---
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/config/DynamoConfig.java
@@ -0,0 +1,19 @@
+package com.eduardoinacio.SpringAWS_Gamehistory_Microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration
+public class DynamoConfig {
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.SA_EAST_1)
+                .build();
+    }
+}
