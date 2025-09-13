a8f97d3d400374058503cd3c791919e5c73201b2
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-12 21:33:38 -0300

    Refactoring Entity-DTO transformation to mapper.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/controller/DTO/GameStatsRequest.java
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/controller/DTO/GameStatsRequest.java
@@ -1,7 +1,5 @@
 package com.eduardoinacio.SpringAWS_Gamehistory_Microservice.controller.DTO;
 
-import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.entity.GameHistoryEntity;
-
 import java.time.Instant;
 
 public record GameStatsRequest(String username, String gameId, Double score, Instant createdAt) {
