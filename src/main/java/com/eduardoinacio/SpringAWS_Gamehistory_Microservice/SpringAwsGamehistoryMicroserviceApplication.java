c0aadc3849d9a1faf0d339dd0f75fe53d0bbffd8
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 04:17:27 -0300

    Enabling scheduling.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/SpringAwsGamehistoryMicroserviceApplication.java
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/SpringAwsGamehistoryMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.eduardoinacio.SpringAWS_Gamehistory_Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringAwsGamehistoryMicroserviceApplication {
 
 	public static void main(String[] args) {
