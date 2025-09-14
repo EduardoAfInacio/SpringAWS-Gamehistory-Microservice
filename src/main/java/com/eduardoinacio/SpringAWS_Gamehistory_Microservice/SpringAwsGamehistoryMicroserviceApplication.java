40c9314850b97c8da22f1bf1ac5ca82881e881d8
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 09:04:00 -0300

    Enabling async.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/SpringAwsGamehistoryMicroserviceApplication.java
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/SpringAwsGamehistoryMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.eduardoinacio.SpringAWS_Gamehistory_Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class SpringAwsGamehistoryMicroserviceApplication {
 
 	public static void main(String[] args) {
