d30769ba278f9240a182f55e5bd7ade6833687e3
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 11:06:52 -0300

    Implemented getTopTenGamesByScore method.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/service/LeaderboardService.java
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/service/LeaderboardService.java
@@ -1,8 +1,12 @@
 package com.eduardoinacio.SpringAWS_Gamehistory_Microservice.service;
 
+import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.controller.DTO.GameStatsResponse;
 import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.entity.GameHistoryEntity;
 import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.mapper.GameHistoryMapper;
 import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.producer.SQSProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import io.awspring.cloud.s3.S3Template;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -40,16 +45,25 @@ public class LeaderboardService {
                 .map(gameHistoryMapper::toGameStatsResponse)
                 .toList();
 
-        if(top10.isEmpty()) return CompletableFuture.completedFuture(null);
+        if (top10.isEmpty()) return CompletableFuture.completedFuture(null);
 
-        try{
+        try {
             String json = objectMapper.writeValueAsString(top10);
 
             s3Template.store("leaderboard-bucket", "top10.json", json);
             sqsProducer.newTopTenNotify();
             return CompletableFuture.completedFuture(null);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error while serializing top10 leaderboard");
+        }
+    }
+
+    public List<GameStatsResponse> getTopTenGamesByScores() {
+        String json = s3Template.read("leaderboard-bucket", "top10.json", String.class);
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<GameStatsResponse>>() {});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
