027549535af535d3b427277a77ffdb38fec0ad02
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 09:53:37 -0300

    Creating Scheduler class.

---
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/scheduler/Scheduler.java
@@ -0,0 +1,21 @@
+package com.eduardoinacio.SpringAWS_Gamehistory_Microservice.scheduler;
+
+import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.service.LeaderboardService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class Scheduler {
+    private LeaderboardService leaderboardService;
+
+    public Scheduler(LeaderboardService leaderboardService) {
+        this.leaderboardService = leaderboardService;
+    }
+
+    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.SECONDS)
+    public void analyseTopTenScores(){
+        leaderboardService.updateTopTenLeaderboard();
+    }
+}
