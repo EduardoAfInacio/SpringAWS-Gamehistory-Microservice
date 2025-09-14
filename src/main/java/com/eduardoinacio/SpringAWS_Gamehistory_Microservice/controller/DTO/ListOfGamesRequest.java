784782b050bd3bcf80bc5c62d9a1572773f08e0c
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 10:07:06 -0300

    Deleting ListOfGamesRequest DTO.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/controller/DTO/ListOfGamesRequest.java
+++
@@ -1,8 +0,0 @@
-package com.eduardoinacio.SpringAWS_Gamehistory_Microservice.controller.DTO;
-
-import com.eduardoinacio.SpringAWS_Gamehistory_Microservice.listener.GameStatsRequest;
-
-import java.util.List;
-
-public record ListOfGamesRequest(List<GameStatsRequest> games) {
-}
