e55d3cd011bb82cbd46bdd2ddd4b03fd1a31e7c1
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 03:30:30 -0300

    Adding SQS configuration to AWSConfig class.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/config/AWSConfig.java
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/config/AWSConfig.java
@@ -4,14 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
 @Configuration
-public class DynamoConfig {
+public class AWSConfig {
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.SA_EAST_1)
+                .build();
+    }
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient(){
+        return SqsAsyncClient.builder()
                 .endpointOverride(URI.create("http://localhost:4566"))
                 .region(Region.SA_EAST_1)
                 .build();
