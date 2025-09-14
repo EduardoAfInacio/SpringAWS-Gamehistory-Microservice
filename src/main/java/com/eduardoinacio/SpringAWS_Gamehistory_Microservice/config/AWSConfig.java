7de91575cdd3cc12182d062af8b607d7b6e678be
EduardoAfInacio <eduafinacio@gmail.com>
2025-09-14 09:53:06 -0300

    Configurating s3 client in AWSConfig.

--- src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/config/AWSConfig.java
+++ src/main/java/com/eduardoinacio/SpringAWS_Gamehistory_Microservice/config/AWSConfig.java
@@ -4,16 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
 @Configuration
 public class AWSConfig {
+    private static final String ENDPOINT_OVERRIDE = "http://localhost:4566";
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566"))
+                .endpointOverride(URI.create(ENDPOINT_OVERRIDE))
                 .region(Region.SA_EAST_1)
                 .build();
     }
@@ -21,8 +25,19 @@ public class AWSConfig {
     @Bean
     public SqsAsyncClient sqsAsyncClient(){
         return SqsAsyncClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566"))
+                .endpointOverride(URI.create(ENDPOINT_OVERRIDE))
                 .region(Region.SA_EAST_1)
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client(){
+        return S3Client.builder()
+                .endpointOverride(URI.create(ENDPOINT_OVERRIDE))
+                .region(Region.SA_EAST_1)
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
                 .build();
     }
 }
